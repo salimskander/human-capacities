1346096c1eb73651cff5a69b9863ab34cb224f22
Salim Skander <salim.skander@ynov.com>
2026-04-15 10:23:48 +0200

    chatbot

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,6 +37,9 @@ export default function RootLayout({
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8659475682678440"
           crossOrigin="anonymous"
         />
+        <script type="text/javascript">
+          window.$crisp=[];window.CRISP_WEBSITE_ID="d240d500-f2ef-40ed-bfb2-64d7c17fe3c1";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
+        </script>
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <AuthProviderWrapper>
